52a918cf269c2a767e224f1a318bc0011f20a136
Infinitybeond1 <aksharvadaria9@gmail.com>
2022-01-12 19:02:25 -0500

    :sparkles: Hello World

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div>
+        Hello World
+      </div>
+    </>
+  );
 }
+
 
 export default MyApp
